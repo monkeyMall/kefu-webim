84f4908b48c618354a215c39a789ded672f72459
jasminehou <hjh892084961@163.com>
2018-01-09 11:43:10 +0800

    test

---
+++ travis.sh
@@ -0,0 +1,32 @@
+
+echo git tag: $TRAVIS_TAG
+echo username : env.username
+echo password: env.password
+
+if [ $TRAVIS_TAG ] && [ "$TRAVIS_TAG"x != ""x ]; then
+
+    if [ `echo $TRAVIS_TAG | grep "^plugin_[0-9]\+\.[0-9]\+\.[0-9]\+_final$"` ]; then
+        
+        # nexus
+        echo ''
+        echo '[is a tag] start packing'
+        npm config set registry https://registry.npm.taobao.org
+        cd kefu-webim
+        cd server
+        npm install
+        cd ../
+        npm install
+        npm run build -- --tag-name=$TRAVIS_TAG
+
+     else
+        echo ''
+        echo 'The format of the tag is not correct.'
+
+    fi
+
+else 
+
+echo ''
+echo '[not a tag] exit packing.'
+
+fi
